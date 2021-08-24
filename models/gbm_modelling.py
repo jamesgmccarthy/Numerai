6475a23845fc1a1e373d32126b9b2f4c30207b2e
james <jamesgmccarthy1@gmail.com>
2021-08-24 19:28:21 +0100

    no big change - clean up mostly

--- models/gbm_modelling.py
+++ models/gbm_modelling.py
@@ -2,34 +2,33 @@
 import datetime
 import gc
 
-import datatable as dt
 import joblib
 import lightgbm as lgb
 import neptune
 import neptunecontrib.monitoring.optuna as opt_utils
-import numpy as np
 import optuna
 import xgboost as xgb
 from sklearn.metrics import mean_squared_error
-from data_loading import utils
+
 from data_loading import purged_group_time_series as pgs
+from data_loading import utils
 
 
 def optimize(trial: optuna.trial.Trial, datasets):
-    p = {'learning_rate': trial.suggest_uniform('learning_rate', 1e-4, 1e-1),
-         'max_depth': trial.suggest_int('max_depth', 5, 30),
-         'max_leaves': trial.suggest_int('max_leaves', 5, 50),
-         'subsample': trial.suggest_uniform('subsample', 0.3, 1.0),
+    p = {'learning_rate':    trial.suggest_uniform('learning_rate', 1e-4, 1e-1),
+         'max_depth':        trial.suggest_int('max_depth', 5, 30),
+         'max_leaves':       trial.suggest_int('max_leaves', 5, 50),
+         'subsample':        trial.suggest_uniform('subsample', 0.3, 1.0),
          'colsample_bytree': trial.suggest_uniform('colsample_bytree', 0.3, 1.0),
          'min_child_weight': trial.suggest_int('min_child_weight', 5, 100),
-         'lambda': trial.suggest_uniform('lambda', 0.05, 0.2),
-         'alpha': trial.suggest_uniform('alpha', 0.05, 0.2),
-         'objective': 'regression',
-         'booster': 'gbtree',
-         'tree_method': 'gpu_hist',
-         'verbosity': 1,
-         'n_jobs': 10,
-         'eval_metric': 'mse'}
+         'lambda':           trial.suggest_uniform('lambda', 0.05, 0.2),
+         'alpha':            trial.suggest_uniform('alpha', 0.05, 0.2),
+         'objective':        'regression',
+         'booster':          'gbtree',
+         'tree_method':      'gpu_hist',
+         'verbosity':        1,
+         'n_jobs':           10,
+         'eval_metric':      'mse'}
     print('Choosing parameters:', p)
     scores = []
     sizes = []
@@ -55,19 +54,19 @@ def optimize(trial: optuna.trial.Trial, datasets):
 
 
 def loptimize(trial, datasets):
-    p = {'learning_rate': trial.suggest_uniform('learning_rate', 1e-4, 1e-1),
-         'max_leaves': trial.suggest_int('max_leaves', 5, 100),
+    p = {'learning_rate':    trial.suggest_uniform('learning_rate', 1e-4, 1e-1),
+         'max_leaves':       trial.suggest_int('max_leaves', 5, 100),
          'bagging_fraction': trial.suggest_uniform('bagging_fraction', 0.3, 0.99),
-         'bagging_freq': trial.suggest_int('bagging_freq', 1, 10),
+         'bagging_freq':     trial.suggest_int('bagging_freq', 1, 10),
          'feature_fraction': trial.suggest_uniform('feature_fraction', 0.3, 0.99),
          'min_data_in_leaf': trial.suggest_int('min_data_in_leaf', 50, 1000),
-         'lambda_l1': trial.suggest_uniform('lambda_l1', 0.005, 0.05),
-         'lambda_l2': trial.suggest_uniform('lambda_l2', 0.005, 0.05),
-         'boosting': trial.suggest_categorical('boosting', ['gbdt', 'goss', 'rf']),
-         'objective': 'binary',
-         'verbose': 1,
-         'n_jobs': 10,
-         'metric': 'auc'}
+         'lambda_l1':        trial.suggest_uniform('lambda_l1', 0.005, 0.05),
+         'lambda_l2':        trial.suggest_uniform('lambda_l2', 0.005, 0.05),
+         'boosting':         trial.suggest_categorical('boosting', ['gbdt', 'goss', 'rf']),
+         'objective':        'binary',
+         'verbose':          1,
+         'n_jobs':           10,
+         'metric':           'auc'}
     if p['boosting'] == 'goss':
         p['bagging_freq'] = 0
         p['bagging_fraction'] = 1.0
@@ -95,12 +94,12 @@ def loptimize(trial, datasets):
 
 
 def main():
-    api_token = 'eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5haSIsImFwaV91cmwiOiJodHRwczovL3VpLm5lcHR1bmUuYWkiLCJhcGlfa2V5IjoiYWQxMjg3OGEtMGI1NC00NzFmLTg0YmMtZmIxZjcxZDM2NTAxIn0='
+    api_token = utils.read_api_token()
     neptune.init(api_token=api_token,
                  project_qualified_name='jamesmccarthy65/Numerai')
     data = utils.load_data('data/', mode='train')
     data, target, features, era = utils.preprocess_data(data, nn=True)
-    datasets = {'data': data, 'target': target,
+    datasets = {'data':     data, 'target': target,
                 'features': features, 'era': era}
     print('creating XGBoost Trials')
     xgb_exp = neptune.create_experiment('XGBoost_HPO')
